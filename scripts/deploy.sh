e987d738c4aa83df1d25a9e089ad1c12dedb401a
Usairim Isani <usairim.isani@outlook.com>
2021-09-05 16:55:32 +0500

    Deploy script - need to fix pages custom domain reset

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 npm run build
+cd ./dist
+pwd
 git init
 git add -A
 git commit -m "Building Zing Technologies $(date +'%F-%T')"
-git push  origin main
+
+git push -f https://github.com/UsairimIsani/usairimisani.github.io.git gh-pages
