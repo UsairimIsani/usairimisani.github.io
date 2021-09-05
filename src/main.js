efec8334d83eca7cf365172a637bfc14f1eae07a
Usairim Isani <usairim.isani@outlook.com>
2021-09-05 15:46:58 +0500

    Initializing Agency Portfolio

--- src/main.js
+++ src/main.js
@@ -18,17 +18,17 @@ export default function (Vue, { router, head, isClient }) {
 
   head.meta.push({
     name: 'keywords',
-    content: 'Gridsome,Vue,Tailwind,Tailwind CSS,JavaScript,HTML,CSS,Vue.js,VueJS'
+    content: 'Zing Technologies,Zing Blockchain Technologies,Rust,NEAR,Solana,NFT,Fungible Token,Token,Crypto Coin,ink!,Smart Contract,Defi,Dex,Liquidity Pool,Development'
   })
 
   head.meta.push({
     name: 'description',
-    content: 'Gridsome Portfolio Starter'
+    content: 'Zing Blockchain Technologies, Blockchain Development, Smart Contract Development,Rust'
   })
 
   head.meta.push({
     name: 'author',
-    content: 'Andre Madarang'
+    content: 'Usairim Isani'
   })
 
   head.link.push({
